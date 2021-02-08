d56144d84b730d8bfe60c4f77e787c410a1e898c
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2021-02-08 18:52:02 +1100

    Minor cleanup

--- src/main/java/arg/WildcardItem.java
+++ src/main/java/arg/WildcardItem.java
@@ -18,6 +18,16 @@ public class WildcardItem extends Item {
 
     @Override
     @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamage(int damage) {
+        if (damage == 0) {
+            return itemIcon;
+        } else {
+            return iconArray[MathHelper.clamp_int(damage, 1, 9)];
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
     public void registerIcons(IIconRegister iiconRegister) {
         itemIcon = iiconRegister.registerIcon(baseIconName + "Unique");
         iconArray = new IIcon[10];
@@ -25,15 +35,6 @@ public class WildcardItem extends Item {
 
         for (int i = 1; i <= 9; i++) {
             iconArray[i] = iiconRegister.registerIcon(baseIconName + i);
-        }
-    }
-
-    @SideOnly(Side.CLIENT)
-    public IIcon getIconFromDamage(int damage) {
-        if (damage == 0) {
-            return itemIcon;
-        } else {
-            return iconArray[MathHelper.clamp_int(damage, 1, 9)];
         }
     }
 
