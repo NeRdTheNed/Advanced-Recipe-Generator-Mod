95a3f08e3a095336c3d5696e73836dbf25d629c0
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2021-02-08 16:53:55 +1100

    Move Wildcard icons into assets folder, register icons and display them based on damage values

--- src/main/java/arg/WildcardItem.java
+++ src/main/java/arg/WildcardItem.java
@@ -1,12 +1,40 @@
 package arg;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.item.Item;
+import net.minecraft.util.IIcon;
+import net.minecraft.util.MathHelper;
 
 public class WildcardItem extends Item {
 
+    private static final String baseIconName = "arg:WildcardIcon";
+    private IIcon[] iconArray;
+
     public WildcardItem() {
         setUnlocalizedName("WildcardItem");
-        setTextureName("arg:" + "WildcardItem");
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister iiconRegister) {
+        itemIcon = iiconRegister.registerIcon(baseIconName + "Unique");
+        iconArray = new IIcon[10];
+        iconArray[0] = itemIcon;
+
+        for (int i = 1; i <= 9; i++) {
+            iconArray[i] = iiconRegister.registerIcon(baseIconName + i);
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public IIcon getIconFromDamage(int damage) {
+        if (damage == 0) {
+            return itemIcon;
+        } else {
+            return iconArray[MathHelper.clamp_int(damage, 1, 9)];
+        }
     }
 
 }
